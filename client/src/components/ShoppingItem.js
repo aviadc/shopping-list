8a1619fc979860297a681938a85c28fcfbcff91c
aviadc <coaviadco@gmail.com>
2023-03-06 16:40:34 +0200

    update item

--- client/src/components/ShoppingItem.js
+++ client/src/components/ShoppingItem.js
@@ -1,12 +1,16 @@
+import React from "react";
+import UpdateItem from "./UpdateItem";
 
 
 export default function ShoppingItem(props) {
-
+  const[isUpdateMode,setIsUpdateMode] = React.useState(false)
   return (
     <div className="shopping-item">
+      <button className="del-button" onClick={()=>props.delItem(props.id)}>del</button>
       <h3>{props.amount}</h3>
       <h2>{props.item}</h2>
-      <button className="del-button" onClick={()=>props.delItem(props.id)}>del</button>
+      <button className="update-button" onClick={()=>setIsUpdateMode(prev=>!prev)}>Update</button>
+      <UpdateItem isUpdateMode={isUpdateMode}/>
     </div>
   );
 }
