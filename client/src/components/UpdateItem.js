6bcfccccfe34ba9bc9c2765463b87ed629992a31
aviadc <coaviadco@gmail.com>
2023-03-26 18:37:46 +0300

    merged

---
+++ client/src/components/UpdateItem.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+export default function UpdateItem(props){
+
+    const myStyle = {
+        right: `${props.isUpdateMode ? '-100%' : 0}`,
+        transition: "right 2s ease-in-out",
+    }
+
+    return (
+        <div className="update-item"  style={myStyle}>
+            <form>
+                <input placeholder="item" />
+                <input placeholder="amount"/>
+                <button>update</button>
+                <button>cancel</button>
+            </form>
+        </div>
+    )
+}
